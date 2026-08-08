f9f92e58493a3b8373627dcd6afa24f332e8ce48
Onlinside <futricou@gmail.com>
2026-08-08 18:41:45 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCUZ98Y78ZxCU166qC5EAAupCDPbv0aF9U",
+  authDomain: "login-4e334.firebaseapp.com",
+  projectId: "login-4e334",
+  storageBucket: "login-4e334.firebasestorage.app",
+  messagingSenderId: "937085407465",
+  appId: "1:937085407465:web:c93de7f716be2480c2a078",
+  measurementId: "G-MNNYF7GEN1"
+};
