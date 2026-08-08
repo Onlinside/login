f7361e855ff9c43dc46200d7fea92e55f09a517a
Onlinside <futricou@gmail.com>
2026-08-08 20:38:33 -0300

    Migrate to Firebase SDK v12.1.0 and initialize app
    Updated Firebase SDK to version 12.1.0 and initialized app and auth.

--- firebase.js
+++ firebase.js
@@ -1,10 +1,18 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCUZ98Y78ZxCU166qC5EAAupCDPbv0aF9U",
-  authDomain: "login-4e334.firebaseapp.com",
-  projectId: "login-4e334",
-  storageBucket: "login-4e334.firebasestorage.app",
-  messagingSenderId: "937085407465",
-  appId: "1:937085407465:web:c93de7f716be2480c2a078",
-  measurementId: "G-MNNYF7GEN1"
+    apiKey: "AIzaSyCUZ98Y78ZxCU166qC5EAAupCDPbv0aF9U",
+    authDomain: "login-4e334.firebaseapp.com",
+    projectId: "login-4e334",
+    storageBucket: "login-4e334.firebasestorage.app",
+    messagingSenderId: "937085407465",
+    appId: "1:937085407465:web:c93de7f716be2480c2a078",
+    measurementId: "G-MNNYF7GEN1"
 };
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { app, auth };
